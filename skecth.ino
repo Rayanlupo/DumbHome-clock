9f0a58167f2919f7a5f8072397ae06e794dc5904
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-08 00:08:17 +0200

    Update skecth.ino

--- skecth.ino
+++ skecth.ino
@@ -5,38 +5,76 @@
 RTC_DS1307 rtc;
 TM1637 display(27, 14);
 
-int hours;
-int minutes;
-
+const int buttonPin = 34;  
+bool lastButtonState = HIGH;
+bool currentButtonState = HIGH;
+unsigned long lastDebounceTime = 0;  
+const unsigned long debounceDelay = 50;  
+unsigned long buttonPressTime = 0;
+const unsigned long displayDuration = 10000;  
+bool displayOn = false;
 void setup() {
   Serial.begin(115200);
-  Wire.begin(19, 18); 
+  Wire.begin(19, 18);  
+  
+  pinMode(buttonPin, INPUT_PULLUP); 
+
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
-    while (1); 
+    while (1);  
   }
 
   if (!rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));  
   }
+
   display.init();
   display.set(7);  
 }
 
 void loop() {
-  DateTime now = rtc.now();  
-  hours = now.hour();
-  minutes = now.minute();  
-  displayTime(hours, minutes);
-  delay(1000);
-}
+  int buttonState = digitalRead(buttonPin);  
 
+  if (buttonState != lastButtonState) {
+    lastDebounceTime = millis();  
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+
+    if (buttonState != currentButtonState) {
+      currentButtonState = buttonState;
+      
+      if (currentButtonState == LOW) {
+        DateTime now = rtc.now();  
+        int hours = now.hour();
+        int minutes = now.minute();  
+        displayTime(hours, minutes);
+        displayOn = true;
+      }
+
+    }
+  }
+
+  lastButtonState = buttonState;  
+
+   if (displayOn && (millis() - buttonPressTime >= displayDuration)) {
+    displayOn = false; 
+    clearDisplay();
+}
+}
 void displayTime(int hours, int minutes) {
   int8_t displayData[4];
+  
+
   displayData[0] = hours / 10;  
   displayData[1] = hours % 10;  
   displayData[2] = minutes / 10;  
   displayData[3] = minutes % 10; 
+
   display.display(displayData);
+}
+void clearDisplay() {
+  int8_t clearData[4] = { 0x7F, 0x7F, 0x7F, 0x7F }; 
+  display.display(clearData);
 }
