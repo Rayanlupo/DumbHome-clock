73c434177774437d5f960c59bbf35b45c547e47f
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-07 22:31:20 +0200

    Update skecth.ino

--- skecth.ino
+++ skecth.ino
@@ -1,55 +1,42 @@
+#include <Wire.h>
 #include <RTClib.h>
 #include <TM1637.h>
+
 RTC_DS1307 rtc;
-TM1637 display(27, 14); 
-int buttonPin = 35;
+TM1637 display(27, 14);
+
 int hours;
 int minutes;
-int show;
-int on;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(buttonPin, INPUT_PULLUP);
-  rtc.begin();
-  display.set(); 
-  display.init(); 
-   if (!rtc.begin()) {
-    Serial.println("RTC not found");
+  Wire.begin(19, 18); 
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1); 
   }
-  if (rtc.isrunning() == false) {
-    Serial.println("RTC is not running");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); 
+
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));  
   }
-  display.set(7);
-
-
+  display.init();
+  display.set(7);  
 }
 
 void loop() {
-
-DateTime now = rtc.now();
-hours = (now.hour());
-int minutes = (now.minute());
-Serial.print(hours);
-Serial.print(":");
-Serial.print(minutes);
-displayTime(hours, minutes);   
-delay(1000);
-
+  DateTime now = rtc.now();  
+  hours = now.hour();
+  minutes = now.minute();  
+  displayTime(hours, minutes);
+  delay(1000);
 }
-
 
 void displayTime(int hours, int minutes) {
   int8_t displayData[4];
-
-
-  displayData[0] = hours / 10;
-  displayData[1] = hours % 10;
-  displayData[2] = minutes / 10;
-  displayData[3] = minutes % 10;
-
-
+  displayData[0] = hours / 10;  
+  displayData[1] = hours % 10;  
+  displayData[2] = minutes / 10;  
+  displayData[3] = minutes % 10; 
   display.display(displayData);
 }
-
